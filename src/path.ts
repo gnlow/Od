fe6c4c0fdc33b92d6a6b962317546fe2a508cda6
Gnlow <gnlow@protonmail.com>
2023-07-09 23:39:35 +0900

    turtle!

--- src/path.ts
+++ src/path.ts
@@ -1,39 +1,64 @@
+type Controler = {
+    move: () => void
+    turnCW: () => void
+    turnCCW: () => void
+    push: () => void
+    pop: () => void
+
+    pos: Vec2
+    dir: Vec2
+}
+
+const turtle = (turtle: (controler: Controler) => void) => {
+    let pos = c(0, 0)
+    const dir = c(-1, 0)
+
+    const move = () => {
+        pos = add(pos, dir)
+    }
+    
+    const turnCW = () => {
+        const {x, y} = dir
+        dir.x = y
+        dir.y = -x
+    }
+    
+    const turnCCW = () => {
+        const {x, y} = dir
+        dir.x = -y
+        dir.y = x
+    }
+
+    const push = () => {
+        stack.push(copy({pos, dir}))
+    }
+    
+    const pop = () => {
+        drawLine(stack.pop()!, {pos, dir})
+    }
+
+    turtle({
+        move,
+        turnCW,
+        turnCCW,
+        push,
+        pop,
+        pos,
+        dir,
+    })
+}
+
 const c =
     (x: number, y: number) =>
     ({x, y})
 
-const pos = c(0, 0)
-const dir = c(-1, 0)
-
-const move = () => {
-    pos.x += dir.x
-    pos.y += dir.y
-}
-
-const turnCW = () => {
-    const {x, y} = dir
-    dir.x = y
-    dir.y = -x
-}
-
-const turnCCW = () => {
-    const {x, y} = dir
-    dir.x = -y
-    dir.y = x
-}
+const add = (a: Vec2, b: Vec2) => c(a.x + b.x, a.y + b.y)
 
 type Vec2 = {x: number, y: number}
 type Point = {pos: Vec2, dir: Vec2}
 
 const stack: Point[] = []
 
-const push = () => {
-    stack.push(copy({pos, dir}))
-}
-
-const pop = () => {
-    drawLine(stack.pop()!, {pos, dir})
-}
 
 const dirToArrow =
     ({x, y}: Vec2) =>
@@ -44,8 +69,15 @@ const lines: [Point, Point][] = []
 const copy = (object: any) => JSON.parse(JSON.stringify(object))
 
 const drawLine =
-    (from: Point, to: Point) =>
-    lines.push([copy(from), copy(to)])
+    (from: Point, to: Point) => {
+        lines.push([copy(from), copy(to)])
+/*
+        const currPos = add(from.pos, from.dir)
+        const currDir = from.dir
+        while (true) {
+
+        }*/
+    }
 
 const posToCoord =
     ({x, y}: Vec2) =>
@@ -64,31 +96,33 @@ class Grid<T> {
     }
 }
 
-const grid = new Grid<boolean>()
-grid.set(pos, true)
+turtle(({move, turnCW, turnCCW, push, pop, pos, dir,}) => {
+    const grid = new Grid<boolean>()
+    grid.set(pos, true)
 
-for (const char of "((-)xx-)") {
-    console.log(char, dirToArrow(dir), pos)
-    if (char == "-") {
-        push()
-        move()
-        grid.set(pos, true)
-        turnCW()
-        turnCW()
-        pop()
-        turnCW()
-    } else {
-        if (char == "(") {
+    for (const char of "((-)xx-)") {
+        console.log(char, dirToArrow(dir), pos)
+        if (char == "-") {
             push()
-        }
-        if (char == ")") {
+            move()
+            grid.set(pos, true)
+            turnCW()
+            turnCW()
             pop()
+            turnCW()
+        } else {
+            if (char == "(") {
+                push()
+            }
+            if (char == ")") {
+                pop()
+            }
+            if (char == "x") {
+                //
+            }
+            turnCW()
         }
-        if (char == "x") {
-            //
-        }
-        turnCW()
     }
-}
 
-console.log(lines)
+    console.log(lines)
+})
