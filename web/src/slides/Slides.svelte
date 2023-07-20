490fd524d05ebac12dfcd248479e4044da2ebe63
Gnlow <gnlow@protonmail.com>
2023-07-20 12:37:33 +0900

    numbering slides

--- web/src/slides/Slides.svelte
+++ web/src/slides/Slides.svelte
@@ -2,16 +2,16 @@
     import SlideContainer from "$lib/SlideContainer.svelte"
     import Slide from "$lib/Slide.svelte"
 
-    import title from "./title.svelte"
-    import doodle from "./doodle.svelte"
-    import def from "./def.svelte"
-    import feature from "./feature.svelte"
-    import encode from "./encode.svelte"
-    import cards1 from "./cards1.svelte"
-    import cards2 from "./cards2.svelte"
-    import limit from "./limit.svelte"
-    import feel from "./feel.svelte"
-    import end from "./end.svelte"
+    import title from "./1_title.svelte"
+    import doodle from "./2_doodle.svelte"
+    import def from "./3_def.svelte"
+    import feature from "./4_feature.svelte"
+    import encode from "./5_encode.svelte"
+    import cards1 from "./6_cards1.svelte"
+    import cards2 from "./7_cards2.svelte"
+    import limit from "./8_limit.svelte"
+    import feel from "./9_feel.svelte"
+    import end from "./10_end.svelte"
 
     const slides = [
         title,
